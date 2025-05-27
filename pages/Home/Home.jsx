f7a3844a511bed40cf950b4f5775b9c042b78231
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-05-27 17:27:48 +0530

    Initial Functionality Work

---
+++ pages/Home/Home.jsx
@@ -0,0 +1,127 @@
+import {React,useState,useEffect} from "react";
+
+
+function Home() {
+
+    const [balance,setBalance]= useState(0);
+    const [expenses,setExpenses]=useState(0);
+    const [expenseList,setExpenseList]=useState([]);
+    const [isMounted,setIsMounted]=useState(false);
+
+
+    const[categorySpends,setCategorySpends]=useState({
+        food:0,
+        entertainment:0,
+        travel:0
+    });
+
+    const[categoryCount,setCategoryCount]=useState({
+        food:0,
+        entertainment:0,
+        travel:0
+    });
+
+
+
+
+    useEffect(()=>{
+        const localbalance=localStorage.getItem("balance");
+
+        if(localbalance)
+        {
+            setBalance(Number(localbalance));
+        }
+        else{
+            setBalance(5000);
+            localStorage.setItem("balance",5000);
+        }
+
+        const items=JSON.parse(localStorage.getItem("expenses"));
+
+        setExpenseList(items || []);
+        setIsMounted(true);
+    },[])
+
+
+    useEffect(()=>{
+        if(expenseList.length>0 || isMounted==true)
+        {
+            localStorage.setItem("expenses",JSON.stringify(expenseList));
+        }
+
+
+        if(expenseList.length>0)
+        {
+            setExpenses(
+                expenseList.reduce((accumulator,currentValue)=>{
+                    return accumulator+Number(currentValue.price);
+            },0)
+            );
+        }
+        else{
+            setExpenses(0);
+        }
+
+        let foodSpends=0,entertainmentSpends=0,travelSpends=0;
+        let foodCount=0,entertainmentCount=0,travelCount=0;
+
+        expenseList.forEach((item)=>{
+            if(item.category==="food")
+            {
+                foodSpends+=Number(item.price);
+                foodCount++;
+            }
+            else if(item.category==="entertainment")
+            {
+                entertainmentSpends+=Number(item.price);
+                entertainmentCount++;
+            }
+            else{
+                travelSpends+=Number(item.price);
+                travelCount++;
+            }
+        });
+
+
+        setCategorySpends({
+            food:foodSpends,
+            entertainment:entertainmentSpends,
+            travel:travelSpends
+        });
+
+        setCategoryCount({
+            food:foodCount,
+            entertainment:entertainmentCount,
+            travel:travelCount
+        });
+
+    },[expenseList]);
+
+    useEffect(()=>{
+        if(isMounted)
+        {
+            localStorage.setItem("balance",balance);
+        }
+    },[balance])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "../pages/Home/Home";
+import {SnackbarProvider} from "notistack";
+
+function App() {
+
+  return (
+      <SnackbarProvider>
+          <div>
+            <Home />
+          </div>
+      </SnackbarProvider>
+  );
+}
+
+export default App
